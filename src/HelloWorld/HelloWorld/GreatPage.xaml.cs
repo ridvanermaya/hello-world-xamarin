abb39583348dfd5341cbf18ce8c99b25b23c561a
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-09-06 13:25:01 -0500

    Project Created, added a custom page and a method to show an alert.

---
+++ src/HelloWorld/HelloWorld/GreatPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class GreatPage : ContentPage
+    {
+        public GreatPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            DisplayAlert("Title", "Hello World", "Ok");
+        }
+    }
+}
