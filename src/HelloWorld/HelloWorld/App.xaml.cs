0a28d65759e533b307e11573ed9405d213a6f32b
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-09-12 12:53:51 -0500

    Finished Grid Example Pages

--- src/HelloWorld/HelloWorld/App.xaml.cs
+++ src/HelloWorld/HelloWorld/App.xaml.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            MainPage = new StackLayoutExerciseTwo();
+            MainPage = new GridExerciseTwo();
         }
 
         protected override void OnStart()
---
+++ src/HelloWorld/HelloWorld/GridExerciseOne.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace HelloWorld
+{
+    public partial class GridExerciseOne : ContentPage
+    {
+        public GridExerciseOne()
+        {
+            InitializeComponent();
+        }
+    }
+}
